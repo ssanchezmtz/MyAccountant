c2af99f4e676825463b0a25485fd410440ab5227
Sergio Sánchez <sergiohsanchezmtz@gmail.com>
2022-08-08 22:29:45 -0500

    Create global.php
    Adding global variables for payments

---
+++ config/global.php
@@ -0,0 +1,6 @@
+<?php 
+
+return [	
+	'PAYMENT_FRECUENCY' => ['Weekly','Biweekly','Monthly','Bimonthly','Quarterly','Bianual','Anual'],
+	'WEEKDAYS' => ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday'],
+];
